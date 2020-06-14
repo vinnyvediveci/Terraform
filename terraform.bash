8c7946910e69f33cae08ce9374ad2120468b31b0
vinnyvediveci <42522408+vinnyvediveci@users.noreply.github.com>
2020-06-14 09:59:25 +0100

    Update terraform.bash

--- terraform.bash
+++ terraform.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Downloads and installs terraform
+
+
 remote_zip="https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip"
 local_zip="/tmp/terraform.zip"
 wget "${remote_zip}" -O "${local_zip}"
